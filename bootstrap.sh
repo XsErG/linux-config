c82db31793abff6c128000071933526ffc9d86cf
Sergey Andreyev <san.andreyev@gmail.com>
2013-06-07 18:27:50 +0300

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,15 @@
 [ -f ~/.gitconfig      ] && mv ~/.gitconfig      ~/.gitconfig.old
 [ -f ~/.xinitrc        ] && mv ~/.xinitrc        ~/.xinitrc.old
 [ -f ~/.xprofile       ] && mv ~/.xprofile       ~/.xprofile.old
+[ -f ~/.profile        ] && mv ~/.profile        ~/.profile.old
+[ -f ~/.gemrc          ] && mv ~/.gemrc          ~/.gemrc.old
 
 git clone https://github.com/XsErG/linux-config ~/.linux-config && \
-  ln -s ~/.linux-config/awesome ~/.config/awesome && \
-  ln -s ~/.linux-config/fonts ~/.fonts && \
-  ln -s ~/.linux-config/gitconfig ~/.gitconfig && \
-  ln -s ~/.linux-config/xinitrc ~/.xinitrc && \
-  ln -s ~/.linux-config/xprofile ~/.xprofile && \
-  ln -s ~/.linux-config/Xresources ~/.Xresources
+  ln -s ~/.linux-config/awesome    ~/.config/awesome  && \
+  ln -s ~/.linux-config/fonts      ~/.fonts           && \
+  ln -s ~/.linux-config/gitconfig  ~/.gitconfig       && \
+  ln -s ~/.linux-config/xinitrc    ~/.xinitrc         && \
+  ln -s ~/.linux-config/xprofile   ~/.xprofile        && \
+  ln -s ~/.linux-config/Xresources ~/.Xresources      && \
+  ln -s ~/.linux-config/profile    ~/.profile         && \
+  ln -s ~/.linux-configgemrc       ~/.gemrc
