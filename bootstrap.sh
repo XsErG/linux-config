0c26742aa021a98dd37aefe991c4302bf6466796
Sergey Andreyev <san.andreyev@gmail.com>
2013-06-07 17:03:28 +0300

    Added bootstrap.sh file for setup config

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Backup exists directories
+[ -d ~/.linux-config   ] && mv ~/.linux-config   ~/.linux-config.old
+[ -d ~/.config/awesome ] && mv ~/.config/awesome ~/.config/awesome.old
+[ -d ~/.fonts          ] && mv ~/.fonts          ~/.fonts.old
+
+# Backup exists files
+[ -f ~/.Xresources     ] && mv ~/.Xresources     ~/.Xresources.old
+[ -f ~/.gitconfig      ] && mv ~/.gitconfig      ~/.gitconfig.old
+[ -f ~/.xinitrc        ] && mv ~/.xinitrc        ~/.xinitrc.old
+[ -f ~/.xprofile       ] && mv ~/.xprofile       ~/.xprofile.old
+
+git clone https://github.com/XsErG/linux-config ~/.linux-config && \
+  ln -s ~/.linux-config/awesome ~/.config/awesome && \
+  ln -s ~/.linux-config/fonts ~/.fonts && \
+  ln -s ~/.linux-config/gitconfig ~/.gitconfig && \
+  ln -s ~/.linux-config/xinitrc ~/.xinitrc && \
+  ln -s ~/.linux-config/xprofile ~/.xprofile && \
+  ln -s ~/.linux-config/Xresources ~/.Xresources
